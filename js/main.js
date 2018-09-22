4c85ed243a21a5ae952073c4a0a5d5fb8d62629f
JOSUE RIVERA <josuerivera@JOSUEs-MacBook-Air.local>
2018-09-21 19:42:33 -0700

    first commit

--- js/main.js
+++ js/main.js
@@ -10,73 +10,13 @@ var zodiacSigns = [
 		dates: Jan 20 - Feb 18
 		good:
 		bad:
-	},
-	{
-		sign: Pisces: The Fish
-		dates: Feb 19 - Mar 20
-		good:
-		bad:
-	},
-	{
-		sign: Aries: The Ram
-		dates: Mar 21 - Apr 19
-		good:
-		bad:
-	},
-	{
-		sign: Taurus: The Bull
-		dates: Apr 20 - May 20
-		good:
-		bad:
-	},
-	{
-		sign: Gemini: The Twins
-		dates: May 21 - Jun 20
-		good:
-		bad:
-	},
-	{
-		sign:Cancer: The Crab
-		dates: June 21 - July 22
-		good:
-		bad:
-	},
-	{
-		sign:Leo: The Lion
-		dates: July 23 - Aug 22
-		good:
-		bad:
-	},
-	{
-		sign: Virgo: The Virgin
-		dates: Aug 23 - Sep 22
-		good:
-		bad:
-	},
-	{
-		sign: Libra: The Scales
-		dates: Sep 23 - Oct 22
-		good:
-		bad:
-	},
-	{
-		sign: Scorpio: The Scorpion
-		dates: Oct 23 - Nov 21
-		good:
-		bad:
-	},
-	{
-		sign: Sagittarius: The Archer
-		dates: Nov 22 - Dec 21
-		good:
-		bad:
 	}
 ];
 
-var input = document.getElementById("zodiacSigns");
-var myH1 = document.getElementById("dates");
-var myH2 = document.getElementById("good");
-var myH3 = document.getElementById("bad");
+var input = document.getElementById("");
+var myH1 = document.getElementById("");
+var myH2 = document.getElementById("");
+var myH3 = document.getElementById("");
 
 function getInfo() {
 	for(var i = 0; i < zodiacSigns.length; i++) {
